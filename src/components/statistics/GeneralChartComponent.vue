261babea6269112517aa76ff3ce0cd6bdbed7651
MarcelPascaru <marcelpascaru85@gmail.com>
2022-11-29 13:23:59 +0200

    Add statistics charts.

---
+++ src/components/statistics/GeneralChartComponent.vue
@@ -0,0 +1,106 @@
+<template>
+  <LineChartGenerator
+      :chart-options="chartOptions"
+      :chart-data="chartData"
+      :chart-id="chartId"
+      :dataset-id-key="datasetIdKey"
+      :plugins="plugins"
+      :css-classes="cssClasses"
+      :styles="styles"
+      :width="width"
+      :height="height"
+  />
+</template>
+
+<script>
+import { Line as LineChartGenerator } from 'vue-chartjs/legacy'
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  CategoryScale,
+  PointElement
+} from 'chart.js'
+
+ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    LinearScale,
+    CategoryScale,
+    PointElement
+)
+
+export default{
+  name: 'LineChart',
+  components: {
+    LineChartGenerator
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: 'line-chart'
+    },
+    datasetIdKey: {
+      type: String,
+      default: 'label'
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 400
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object,
+      default: () => {}
+    },
+    plugins: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      chartData: {
+        labels: [
+          '2016',
+          '2017',
+          '2018',
+          '2019',
+          '2020',
+          '2021',
+          '2022'
+        ],
+        datasets: [
+          {
+            label: 'Incomes',
+            backgroundColor: '#d01c1c',
+            data: [40, 39, 10, 40, 39, 80, 40],
+          },
+          {
+            label: 'Costs',
+            backgroundColor: '#20276c',
+            data: [60, 20, 25, 67, 32, 76, 35],
+          }
+        ]
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }
+  }
+}
+</script>
---
+++ src/view/statistics/Statistic.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <h1 class="view-title">Incomes Chart Statistic</h1>
+  <BarChart/>
+  <h1 class="view-title">Incomes and Costs Chart Statistic</h1>
+  <LineChart/>
+  <h1 class="view-title">Costs Chart Statistic</h1>
+  <PieChart/>
+</div>
+</template>
+
+<script>
+import BarChart from "@/components/statistics/IncomesChartComponent";
+import LineChart from "@/components/statistics/GeneralChartComponent";
+import PieChart from "@/components/statistics/CostsChartComponent";
+
+
+
+export default {
+  name: "StatisticView",
+  components: {PieChart, LineChart, BarChart}
+}
+</script>
+
+<style scoped>
+
+</style>
